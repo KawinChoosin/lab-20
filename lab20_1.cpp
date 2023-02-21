44f0f6bb0d5522242fec9dbf50df84c2f3082b9a
Your Name <kawin_chusin@cmu.ac.th>
2023-02-21 17:57:11 +0700

    edit

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -5,8 +5,18 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect r,Rect v){
+	double x,y;
+	double up,down,left,right;
+	up=min(r.y,v.y);
+	down=max(r.y-r.h,v.y-v.h);
+	y=up-down;
+	right=max(r.x,v.x);
+	left=min(r.x+r.w,v.x+v.w);
+	x=left-right;
+	if(x<0 or y<0){
+		return 0;
+	}else{
+		return x*y;
+	}
 }
